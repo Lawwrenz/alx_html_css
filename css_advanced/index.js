911d1db84df88e2679fa89f90e93713fbc4e8f87
Lawrence Ezealor <lawrenceezealor1@gmail.com>
2025-06-10 13:51:22 +0100

    updated the css content

---
+++ css_advanced/index.js
@@ -0,0 +1,13 @@
+function  filterEvenNumbers([1,2,3,4,5,6,7,8,9,10]){
+    const evenNumbers = [];
+
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] % 2 === 0){
+            evenNumbers.push(numbers[i])
+        }
+    }
+
+    return evenNumbers;
+}
+
+console.log(result)
